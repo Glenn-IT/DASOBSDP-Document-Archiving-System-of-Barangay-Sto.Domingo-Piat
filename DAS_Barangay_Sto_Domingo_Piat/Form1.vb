e5b93a94bc718cef27ceadbe731e8c90cf0e3243
Glenn-IT <glenard2308@gmail.com>
2026-03-29 18:27:03 +0800

    Add project files.

---
+++ DAS_Barangay_Sto_Domingo_Piat/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim checklist As New frmDocumentChecklist()
+        checklist.Show()
+        Me.Hide()
+    End Sub
+
+End Class
